64ea7040cb1577b043a515fb5a43f2c71d765f57
catyugu <1435221207@qq.com>
2025-06-08 14:20:39 +0000

    commit

---
+++ src/compile-run.sh
@@ -0,0 +1,8 @@
+# Compile and run a certain cpp file using g++
+full_file_name=$1
+out_file_name=../obj/${full_file_name%.*}
+g++ -std=c++17 $full_file_name -o $out_file_name
+echo "Compiled $full_file_name"
+echo "Running $out_file_name"
+./$out_file_name
+echo "Done"
